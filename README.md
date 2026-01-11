c2219f0e2b0f78d11460aa69ecd0745893b1578a
lohikrme <chinese.parrotlet@gmail.com>
2026-01-11 18:23:02 +0200

    add states for clearer dice rolls

--- README.md
+++ README.md
@@ -1,28 +1,35 @@
 # Roll20-Dice-Roller
-Hello! This is my Roll20 Dice Roller version 1.0.2. 
-For personal use only.
-Last time updated: 19th april 2025 by lohikrme
 
+Hello! This is my Roll20 Dice Roller version 1.0.2.
+For personal use only.
+Last time updated: 11th january 2026 by lohikrme
 
 ## Where and how to use it?:
 
 ![alt text](images/image.png)
-- This code is intended to be used only in app.roll20.net/editor website
-- If this code is enabled on the browser and running while gaming at roll20 website, the player can just click a number between 1 and 9 on their keyboard to roll d6 dices
-- To pause rolling (e.g needs write normal numbers) the player can click the 'ESC' button on their keyboard to toggle rolling on/off. 
-- as seen in image above, if dice roller is turned on, its logo will appear on right up corner of the chat
+
+-   This code is intended to be used only in app.roll20.net/editor website
+-   If this code is enabled on the browser and running while gaming at roll20 website, the player can just click a number between 1 and 9 on their keyboard to roll d6 dices
+-   To pause rolling (e.g needs write normal numbers) the player can click the 'ESC' button on their keyboard to toggle rolling on/off.
+-   as seen in image above, if dice roller is turned on, its logo will appear on right up corner of the chat
+-   u can press letters 'd', 's', 'w', 'o', 'p', 'n' to determine, what kind of roll it is. This will change the state and therehow add a text after the roll. Different states are:
+-   FIGHT
+-   SHOOT
+-   WOUND
+-   OUTCOME
+-   PRIORITY
+-   NONE/NULL
 
 ## How to enable the code?:
 
 ![alt text](images/inspector.png)
-- Open web browser and press F12. This opens an inspector for you. 
-- Find inside inspector "Sources" -> "Snippets" -> "New snippets" to create a new code
-- After copying and pasteing this code inside the new snippet and saving (for example with control + S), it will be saved on your browser inspector.
-- To start the code, open the roll20 website where u plan to play, and there open the inspector with F12, go to "Sources" -> "Snippets" and right click the snippet u already created. Select "Run".
-- This code will automatically turn off after closing the browser or the web page.
 
-
+-   Open web browser and press F12. This opens an inspector for you.
+-   Find inside inspector "Sources" -> "Snippets" -> "New snippets" to create a new code
+-   After copying and pasteing this code inside the new snippet and saving (for example with control + S), it will be saved on your browser inspector.
+-   To start the code, open the roll20 website where u plan to play, and there open the inspector with F12, go to "Sources" -> "Snippets" and right click the snippet u already created. Select "Run".
+-   This code will automatically turn off after closing the browser or the web page.
 
 ## Is it safe?:
 
-This code is super safe. It does not store or send any kind of information, and it will work only on the web page it was turned on. E.g if you open roll20 website turn the snippet on there, other pages will act normally. 
+This code is super safe. It does not store or send any kind of information, and it will work only on the web page it was turned on. E.g if you open roll20 website turn the snippet on there, other pages will act normally.
--- roll20_dice_roller.js
+++ roll20_dice_roller.js
@@ -6,6 +6,17 @@
 // init variables
 let receiving_rolls = true;
 
+const states = {
+    FIGHT: 'fight',
+    PRIORITY: 'priority',
+    SHOOT: 'shoot',
+    WOUND: 'wound',
+    OUTCOME: 'outcome',
+    NONE: '',
+};
+
+let state = states.NONE;
+
 // Select the roll20.net chatbox with id and class where diceroller writes
 let textarea = document.querySelector('#textchat-input .ui-autocomplete-input');
 let send_button = document.querySelector('#textchat-input .btn');
@@ -22,74 +33,150 @@ diceImage.style.position = 'absolute';
 diceImage.style.top = '50px';
 diceImage.style.right = '50px';
 
+const stateText = document.createElement('p');
+stateText.style.height = 'auto';
+stateText.style.width = '100px';
+stateText.style.position = 'absolute';
+stateText.style.top = '160px';
+stateText.style.right = '50px';
+stateText.style.fontSize = '2em';
+stateText.style.lineHeight = '1.5';
+stateText.style.color = '#228B22';
+stateText.style.fontFamily = 'Papyrus, serif';
+stateText.innerText = `State: None`;
+
 let rightsidebar = document.querySelector('#rightsidebar .tabmenu');
 rightsidebar.appendChild(diceImage);
+rightsidebar.appendChild(stateText);
+
+// update state and state text
+function updateStateText() {
+    if (state === states.NONE) {
+        stateText.innerText = `State: none`;
+    } else {
+        stateText.innerText = `State: ${state}`;
+    }
+}
 
 //----------Listen to own keyboard events and activate scripts----------
 function KeyUpHandler(event) {
     console.log(event);
 
+    // ------------------
+    // DICE ROLLS
+
     // User presses key 1
     if (event.keyCode === 49 && receiving_rolls) {
-        textarea.value = '/roll 1d6';
+        textarea.value = `/roll 1d6 ${state}`;
         send_button.click();
     }
 
     // User presses key 2
     else if (event.keyCode === 50 && receiving_rolls) {
-        textarea.value = '/roll 2d6';
+        textarea.value = `/roll 2d6 ${state}`;
         send_button.click();
     }
 
     // User presses key 3
     else if (event.keyCode === 51 && receiving_rolls) {
-        textarea.value = '/roll 3d6';
+        textarea.value = `/roll 3d6 ${state}`;
         send_button.click();
     }
 
     // User presses key 4
     else if (event.keyCode === 52 && receiving_rolls) {
-        textarea.value = '/roll 4d6';
+        textarea.value = `/roll 4d6 ${state}`;
         send_button.click();
     }
 
     // User presses key 5
     else if (event.keyCode === 53 && receiving_rolls) {
-        textarea.value = '/roll 5d6';
+        textarea.value = `/roll 5d6 ${state}`;
         send_button.click();
     }
 
     // User presses key 6
     else if (event.keyCode === 54 && receiving_rolls) {
-        textarea.value = '/roll 6d6';
+        textarea.value = `/roll 6d6 ${state}`;
         send_button.click();
     }
 
     // User presses key 7
     else if (event.keyCode === 55 && receiving_rolls) {
-        textarea.value = '/roll 7d6';
+        textarea.value = `/roll 7d6 ${state}`;
         send_button.click();
     }
 
     // User presses key 8
     else if (event.keyCode === 56 && receiving_rolls) {
-        textarea.value = '/roll 8d6';
+        textarea.value = `/roll 8d6 ${state}`;
         send_button.click();
     }
 
     // User presses key 9
     else if (event.keyCode === 57 && receiving_rolls) {
-        textarea.value = '/roll 9d6';
+        textarea.value = `/roll 9d6 ${state}`;
         send_button.click();
+    }
+
+    // ----------------------------------------------
+    // STATES
+
+    // User presses key "f" to enter "FIGHT" state
+    // ascii 70 = F, ascii 102 = f
+    else if (event.keyCode === 70 || event.keyCode === 102) {
+        state = states.FIGHT;
+        updateStateText();
+    }
+
+    // User presses key "s" to enter "SHOOT" state
+    // ascii 83 = S, ascii 115 = s
+    else if (event.keyCode === 83 || event.keyCode === 115) {
+        state = states.SHOOT;
+        updateStateText();
+    }
+
+    // User presses key "w" to enter "WOUND" state
+    // ascii 87 = W, ascii 119 = w
+    else if (event.keyCode === 87 || event.keyCode === 119) {
+        state = states.WOUND;
+        updateStateText();
+    }
+
+    // User presses key "o" to enter "OUTCOME" state
+    // ascii 79 = O, ascii 111 = o
+    else if (event.keyCode === 79 || event.keyCode === 111) {
+        state = states.OUTCOME;
+        updateStateText();
+    }
+
+    // User presses key "p" to enter "PRIORITY" state
+    // ascii 80 = P, ascii 112 = p
+    else if (event.keyCode === 80 || event.keyCode === 112) {
+        state = states.PRIORITY;
+        updateStateText();
+    }
+
+    // User presses key "n" to enter "NONE/NULL" default state
+    // ascii 78 = N, ascii 110 = n
+    else if (event.keyCode === 78 || event.keyCode === 110) {
+        state = states.NONE;
+        updateStateText();
     }
 
     // User wants to keep break a from rolls so he can write text with numbers
     else if (event.keyCode === 27) {
         receiving_rolls = receiving_rolls ? false : true;
+        // not receiving rolls, set status to NONE and image to suitable pause-image
         if (!receiving_rolls) {
+            state = states.NONE;
+            updateStateText();
+
             diceImage.src =
                 'https://raw.githubusercontent.com/lohikrme/Roll20-Dice-Roller/refs/heads/main/images/dice2-pause.png';
-        } else {
+        }
+        // receiving images, set image to suitable active-image
+        else {
             diceImage.src =
                 'https://raw.githubusercontent.com/lohikrme/Roll20-Dice-Roller/refs/heads/main/images/dice.png';
         }
